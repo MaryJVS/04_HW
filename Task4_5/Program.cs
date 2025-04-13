ab045963cf1a444c618de2c4299413e6eaf0bcdf
vasilieva.maria98@gmail.com <vasilieva.maria98@gmail.com>
2025-04-13 16:35:25 +0300

    fix

---
+++ Task4_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число из диапазона от 20 до 60 включительно");
+            int a = Convert.ToInt32(Console.ReadLine());
+            while (a < 20 || a > 60)
+            {
+                Console.WriteLine("Введите корректное число");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Число введено корректное");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4_7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = n;
+            int i = 0;
+
+            while (m > 0)
+            {
+                m = m / 10;
+                i++;
+            }
+
+            Console.WriteLine("Введенное число {0} состоит из {1} цифр.", n, i);
+            Console.ReadKey();
+
+        }
+    }
+}
